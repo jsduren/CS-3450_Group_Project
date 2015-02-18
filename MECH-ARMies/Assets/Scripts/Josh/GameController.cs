2055fc3b1f646806a681d53fdf51a2ccd142f850
Joshua Duren <jsduren@hotmail.com>
2015-02-17 17:04:28 -0700

    What I was working on
    Contains the Terrain, a start for the GameController, and the beginnings
    of the GUI.  I also Downloaded some assets that I added to go over that
    might be useful

---
+++ MECH-ARMies/Assets/Scripts/Josh/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	private bool gameOverPlayer1;
+	private bool gameOverPlayer2;
+
+	// Use this for initialization
+	void Start () {
+		gameOverPlayer1 = false;
+		gameOverPlayer2 = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
