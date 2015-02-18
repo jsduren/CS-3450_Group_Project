e2ae3c423f3c3c535dac75b6cb74b2ff16cf8178
Joshua Duren <jsduren@hotmail.com>
2015-02-18 06:20:21 -0700

    More of the Map, Buildings, and Design Modules Done
    Got the Map more complete and fixed the bridges.  Created the Buildings
    and their prefabs.  Started on the scripts for the Buildings.  I have
    the Game Pitch and Game Modules in some word docs in the Design
    Documents Folder

---
+++ MECH-ARMies/Assets/Scripts/Josh/Base Scripts/MainBaseAttributes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainBaseAttributes : MonoBehaviour {
+	public bool setTeam;		// True = Team 1  False = Team 2
+	public bool currentTeam; 	// True = Team 1  False = Team 2
+	public int startingBaseHealth = 1000;
+	public int currentBaseHealth;
+	public Image damageImage;
+	public Slider healthSlider;
+	public AudioClip explosionClip;
+
+	Animator anim;
+	AudioSource baseDamagedAudio;
+	bool isDead;
+	bool damaged;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		baseDamagedAudio = GetComponent<AudioSource> ();
+		currentBaseHealth = startingBaseHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (damaged) {
+			damageImage.enabled = true;
+		}
+	}
+}
