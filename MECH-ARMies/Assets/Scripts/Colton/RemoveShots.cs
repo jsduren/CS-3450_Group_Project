99ffbba88687592f4a15f584d411a8129ae6d63b
Colton <ColtonParry@gmail.com>
2015-02-21 16:39:26 -0700

    ship, camera, lights

---
+++ MECH-ARMies/Assets/Scripts/Colton/RemoveShots.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveShots : MonoBehaviour 
+
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
+
